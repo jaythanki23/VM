0d0ac35eab937efd25f35d781d31c3588f609805
Jay <thankijay99@gmail.com>
2023-10-12 11:31:19 +0100

    added opcodes and started writing procedure

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 using namespace std;
 
@@ -22,6 +30,91 @@ enum {
 
 uint16_t reg[R_COUNT];
 
-int main() {
+enum {
+  OP_BR = 0,
+  OP_ADD,
+  OP_LD,
+  OP_ST,
+  OP_JSR,
+  OP_AND,
+  OP_LDR,
+  OP_STR,
+  OP_RTI,
+  OP_NOT,
+  OP_LDI,
+  OP_STI,
+  OP_JMP,
+  OP_RES,
+  OP_LEA,
+  OP_TRAP
+};
 
+enum {
+  FL_POS = 1 << 0,
+  FL_ZERO = 1 << 1,
+  FL_NEG = 1 << 2
+};
+
+uint16_t mem_read(uint16_t address) {};
+
+
+int main(int argc, const char* argv[]) {
+  if(argc < 2) {
+    printf("lc3 [image-file1] ...\n");
+    exit(2);
+  }
+
+  for(int j = 1; j < argc; j++) {
+    // read_image
+  }
+
+
+  reg[R_COND] = FL_ZERO;
+
+  enum {PC_START = 0x3000 };
+  reg[R_PC] = PC_START;
+  
+  bool running = true;
+  while(running) {
+    // FETCH
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op = instr >> 12;
+
+    switch (op)
+    {
+      case OP_ADD:
+        break;
+      case OP_AND:
+        break;
+      case OP_NOT:
+        break;
+      case OP_BR:
+        break;
+      case OP_JMP:
+        break;
+      case OP_JSR:
+        break;
+      case OP_LD:
+        break;
+      case OP_LDI:
+        break;
+      case OP_LDR:
+        break;
+      case OP_LEA:
+        break;
+      case OP_ST:
+        break;
+      case OP_STI:
+        break;
+      case OP_TRAP:
+        break;
+      case OP_RES:
+        break;
+      case OP_RTI:
+        break;
+      
+      default:
+        break;
+    }
+  }
 }
